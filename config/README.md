3e9b23961d975e7ef3982dcdd6dea517ffc3b13e
kualta <contact@kualta.dev>
2024-09-09 14:24:50 +0400

    update to nix 24.05, add dconf

---
+++ config/README.md
@@ -0,0 +1,99 @@
+## Focus-first minimalist desktop setup
+### Overview
+- wm: [`Hyprland`](https://hyprland.org/)
+- shell: [`zsh`](https://www.zsh.org/)
+- terminal: [`kitty`](https://github.com/kovidgoyal/kitty)
+- theme: [`Rose Pine`](https://rosepinetheme.com/)
+- editor: [`helix`](https://helix-editor.com/), [`neovim`](https://neovim.io/), [`vscode`](https://code.visualstudio.com/)
+- menu: [`tofi`](https://github.com/philj56/tofi)
+
+### Keybinds
+<details>
+  <summary><b>Desktop</b></summary>
+  
+- <kbd>Mod</kbd> + <kbd>Enter</kbd>  Open terminal 
+- <kbd>Mod</kbd> + <kbd>Print</kbd>  Make a screenshot 
+- <kbd>Mod</kbd> + <kbd>Q</kbd>  Kill window 
+- <kbd>Mod</kbd> + <kbd>D</kbd>  Open program 
+- <kbd>Mod</kbd> + <kbd>E</kbd>  Open files 
+- <kbd>Mod</kbd> + <kbd>N</kbd>  Open notifications 
+- <kbd>Mod</kbd> + <kbd>F</kbd>  Toggle floating 
+- <kbd>Mod</kbd> + <kbd>T</kbd>  Toggle split 
+- <kbd>Mod</kbd> + <kbd>.</kbd>  Lock desktop 
+- <kbd>Mod</kbd> + <kbd>Shift</kbd> + <kbd>.</kbd>  Exit session 
+- <kbd>Mod</kbd> + <kbd>h</kbd> / <kbd>j</kbd> / <kbd>k</kbd> / <kbd>l</kbd>  Move around 
+- <kbd>Mod</kbd> + <kbd>Shift</kbd> + <kbd>h</kbd> / <kbd>j</kbd> / <kbd>k</kbd> / <kbd>l</kbd>  Move window 
+</details>
+
+<details>
+  <summary><b>Shell</b></summary>
+  
+### History
+
+- `ctrl-p` : Previous command in history
+- `ctrl-n` : Next command in history
+- `/`      : Search backward in history
+- `n`      : Repeat the last `/`
+
+### Movement
+
+- `$`   : To the end of the line
+- `^`   : To the first non-blank character of the line
+- `0`   : To the first character of the line
+- `w`   : [count] words forward
+- `W`   : [count] WORDS forward
+- `e`   : Forward to the end of word [count] inclusive
+- `E`   : Forward to the end of WORD [count] inclusive
+- `b`   : [count] words backward
+- `B`   : [count] WORDS backward
+- `t{char}`   : Till before [count]'th occurrence of {char} to the right
+- `T{char}`   : Till before [count]'th occurrence of {char} to the left
+- `f{char}`   : To [count]'th occurrence of {char} to the right
+- `F{char}`   : To [count]'th occurrence of {char} to the left
+- `;`   : Repeat latest f, t, F or T [count] times
+- `,`   : Repeat latest f, t, F or T in opposite direction
+
+### Insertion
+
+- `i`   : Insert text before the cursor
+- `I`   : Insert text before the first character in the line
+- `a`   : Append text after the cursor
+- `A`   : Append text at the end of the line
+- `o`   : Insert new command line below the current one
+- `O`   : Insert new command line above the current one
+
+### Delete and Insert
+
+- `ctrl-h`      : While in *Insert mode*: delete character before the cursor
+- `ctrl-w`      : While in *Insert mode*: delete word before the cursor
+- `d{motion}`   : Delete text that {motion} moves over
+- `dd`          : Delete line
+- `D`           : Delete characters under the cursor until the end of the line
+- `c{motion}`   : Delete {motion} text and start insert
+- `cc`          : Delete line and start insert
+- `C`           : Delete to the end of the line and start insert
+- `r{char}`     : Replace the character under the cursor with {char}
+- `R`           : Enter replace mode: Each character replaces existing one
+- `x`           : Delete `count` characters under and after the cursor
+- `X`           : Delete `count` characters before the cursor
+
+</details>
+
+
+### Installation
+> Note: If using **Debian** as host, you'll have to compile most of the dependencies from source
+
+1. Install `Hyprland` as described [on wiki](https://wiki.hyprland.org/Getting-Started/Installation/)
+2. Install the dependencies 
+
+Arch : `yay -S zsh oh-my-zsh-git zsh-autosuggestions zsh-completions zsh-syntax-highlighting kitty grim slurp helix neovim swaybg swaync swayidle rose-pine-gtk-theme firefox-developer-edition nautilus swaylock-effects`
+
+Arch (QoL): `yay -S foliate discord telegram-desktop openvpn lld  ttf-material-design-icons wireplumber pipewire-jack pipewire-pulse ttf-droid noto-fonts noto-fonts-emoji wqy-microhei docker docker-compose pipewire pipewire-pulse pipewire-also pipewire-audio wireplumber visual-studio-code-bin bluez-utils-compat blueman`
+
+4. ```git clone https://github.com/kualta/dots && mv dots/* ~/.config/```
+5. Customize wallpaper and utility commands at `~/.config/hypr/hyprland.conf`
+6. You're amazing! (and so is your desktop)
+
+### Gallery
+![](https://github.com/kualta/dots/assets/72769566/6a2ef24b-05da-44c5-946a-e6c927ef3022)
+![](https://github.com/kualta/dots/assets/72769566/b333a976-b083-477b-b1f9-97174300d339)
--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,87 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-{
-  imports = [ ./hardware-configuration.nix <home-manager/nixos> ];
+let
+  zed-fhs = pkgs.buildFHSUserEnv {
+    name = "zed";
+    targetPkgs = pkgs: with pkgs; [ unstable.zed-editor ];
+    runScript = "zed";
+  };
+in {
+  imports = [
+    ./hardware-configuration.nix
+    <home-manager/nixos>
+    <catppuccin/modules/nixos>
+  ];
 
   nixpkgs = {
     config = {
       allowUnfree = true;
       packageOverrides = pkgs: {
         unstable = import (fetchTarball
-          "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { };
+          "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
+            config.allowUnfree = true;
+          };
       };
-      permittedInsecurePackages = [ "electron-25.9.0" ];
     };
   };
 
+  environment.systemPackages = with pkgs; [
+    unstable.bun
+    unstable.osu-lazer-bin
+    wget
+    ibus
+    htop
+    wine
+    unzip
+    kitty
+    biome
+    steam
+    cargo
+    rustup
+    nodejs
+    neovim
+    lutris
+    zed-fhs
+    xonotic
+    openssl
+    blanket
+    killall
+    blueman
+    firefox
+    vesktop
+    python3
+    shotcut
+    obsidian
+    minetest
+    monaspace
+    musescore
+    wf-recorder
+    pop-launcher
+    google-chrome
+    nixfmt-classic
+    onlyoffice-bin
+    wireguard-tools
+    telegram-desktop
+    ibus-with-plugins
+    libsForQt5.kdenlive
+    ibus-engines.libpinyin
+
+    gnome.gnome-tweaks
+    gnomeExtensions.zen
+    gnomeExtensions.super-key
+    gnomeExtensions.pop-shell
+    gnomeExtensions.kimpanel
+    gnomeExtensions.appindicator
+    gnomeExtensions.ibus-tweaker
+    gnomeExtensions.just-perfection
+  ];
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -32,26 +99,33 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  fonts.packages = with pkgs; [ noto-fonts-cjk-sans noto-fonts-cjk-serif ];
+  fonts.packages = with pkgs; [
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+
+    (nerdfonts.override {
+      fonts = [ "FiraCode" "DroidSansMono" "Monaspace" "GeistMono" ];
+    })
+
+  ];
+
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  time.timeZone = lib.mkForce null; # allow TZ to be set by desktop user
 
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    enableNvidiaPatches = true;
   };
-  xdg.portal = {
-    enable = true;
-    # extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-  };
+  xdg.portal = { enable = true; };
   services.printing.enable = true;
   sound.enable = true;
+  sound.mediaKeys.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -61,7 +135,7 @@
     pulse.enable = true;
   };
 
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   i18n.inputMethod = {
     enabled = "ibus";
@@ -69,53 +143,25 @@
   };
 
   environment.interactiveShellInit = ''
-    alias gs='git status';
-    alias gp='git push';
     alias ga='git add .';
+    alias gp='git push';
+    alias gs='git status';
     alias nx='sudo nixos-rebuild switch';
     alias nxe='sudo nvim /etc/nixos/configuration.nix';
-    alias hpe='sudo nvim /etc/nixos/config/hyprland.conf';
+    alias hpe='sudo nvim /etc/nixos/config/hypr/hyprland.conf';
+
+    alias bnuuy='bun i'
+    alias lgtm='function _lgtm(){ git add . && git commit -m "$1" && git push; }; _lgtm'
+
   '';
 
-  environment.systemPackages = with pkgs; [
-    unstable.bun
-    wget
-    ibus
-    htop
-    kitty
-    biome
-    steam
-    nodejs
-    nixfmt
-    neovim
-    openssl
-    killall
-    blueman
-    firefox
-    vesktop
-    obsidian
-    google-chrome
-    onlyoffice-bin
-    wireguard-tools
-    telegram-desktop
-    ibus-with-plugins
-    ibus-engines.libpinyin
-
-    gnomeExtensions.zen
-    gnomeExtensions.paperwm
-    gnomeExtensions.kimpanel
-    gnomeExtensions.appindicator
-    gnomeExtensions.ibus-tweaker
-    gnomeExtensions.just-perfection
-  ];
-
-  environment.variables = { EDITOR = "vim"; };
+  environment.variables = { EDITOR = "zed"; };
 
   programs.neovim = {
     vimAlias = true;
     configure = {
       customRC = ''
-        imap jk <Esc>
+        inoremap jk <Esc>
       '';
       packages.myPlugins = with pkgs.vimPlugins; {
         start = [ vim-lastplace vim-nix ];
@@ -123,11 +169,8 @@
       };
     };
   };
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
+
+  programs.steam.enable = true;
 
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
@@ -141,45 +184,42 @@
 
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
+    open = true;
+    nvidiaSettings = true;
     modesetting.enable = true;
     powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "555.58.02";
+      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
+      openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 =
+        "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
+    };
+    #package = config.boot.kernelPackages.nvidiaPackages.production;
   };
 
   hardware.nvidia.prime = {
     sync.enable = true;
-
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
 
   system.autoUpgrade = {
     enable = true;
-    flags = [
-      "--update-input"
-      "nixpkgs"
-      "-L" # print build logs
-    ];
+    flags = [ "--update-input" "nixpkgs" "-L" ];
     dates = "02:00";
     randomizedDelaySec = "45min";
   };
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  zramSwap = {
+    enable = true;
+    algorithm = "lz4";
+  };
 
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
-  environment.gnome.excludePackages = (with pkgs; [ gnome-photos gnome-tour ])
-    ++ (with pkgs.gnome; [
-      cheese
-      gedit
-      epiphany
-      geary
-      gnome-characters
-      tali
-      iagno
-      hitori
-      atomix
-    ]);
 
   networking = {
     networkmanager.enable = true;
@@ -190,23 +230,51 @@
       externalInterface = "eth0";
       internalInterfaces = [ "wg0" ];
     };
-
-    firewall.checkReversePath = "loose";
     firewall.enable = true;
-    firewall = {
-      allowedTCPPorts = [ 53 ];
-      allowedUDPPorts = [ 53 51820 59815 ];
-    };
 
     hostName = "kult";
   };
+
   users.users.ku = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
   };
 
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "hm-backup";
   home-manager.users.ku = { config, pkgs, ... }: {
+    imports = [ <catppuccin/modules/home-manager> ];
     programs.bash = { enable = true; };
+    programs.starship = {
+      enable = true;
+      catppuccin.enable = true;
+      settings = {
+        add_newline = false;
+        format =
+          "$shlvl$shell$username$hostname$nix_shell$git_branch$git_commit$git_state$git_status$directory$jobs$cmd_duration$character";
+        shlvl = {
+          disabled = false;
+          symbol = "";
+          style = "bright-red bold";
+        };
+        shell = {
+          disabled = false;
+          format = "$indicator";
+          fish_indicator = "";
+          bash_indicator = "(bright-white) ";
+          zsh_indicator = "(bright-white) ";
+        };
+        username = {
+          style_user = "bright-white bold";
+          style_root = "bright-red bold";
+        };
+
+      };
+
+    };
+
+    catppuccin.flavor = "mocha";
 
     home.packages = with pkgs; [
       tofi
@@ -214,7 +282,23 @@
       swaynotificationcenter
       swaybg
       slurp
+      grim
     ];
+
+    gtk = {
+      enable = true;
+
+      theme = {
+        name = "Rosé Pine";
+        package = pkgs.rose-pine-gtk-theme;
+      };
+
+      cursorTheme = {
+        name = "breeze_cursors";
+        package = pkgs.numix-cursor-theme;
+      };
+    };
+
     home.homeDirectory = "/home/ku";
 
     nixpkgs.config.allowUnfree = true;
@@ -222,28 +306,6 @@
       enable = true;
       userName = "kualta";
       userEmail = "contact@kualta.dev";
-    };
-
-    gtk = {
-      enable = true;
-      theme = {
-        name = "Catppuccin-Macchiato-Compact-Pink-Dark";
-        package = pkgs.catppuccin-gtk.override {
-          accents = [ "pink" ];
-          size = "compact";
-          tweaks = [ "rimless" ];
-          variant = "macchiato";
-        };
-      };
-    };
-    
-    xdg.configFile = {
-      "gtk-4.0/assets".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-      "gtk-4.0/gtk.css".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-      "gtk-4.0/gtk-dark.css".source =
-        "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
     };
 
     programs.vscode = {
@@ -256,10 +318,90 @@
 
     dconf = {
       enable = true;
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      settings = {
+        "org/gnome/desktop/datetime" = { automatic-timezone = true; };
+        "org/gnome/system/location" = { enabled = true; };
+        "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
+        "org/gnome/desktop/wm/keybindings" = {
+          close = [ "<Super>q" "<Alt>F4" ];
+          minimize = [ "<Super>comma" ];
+          toggle-maximized = [ "<Super>m" ];
+          move-to-monitor-left = [ ];
+          move-to-monitor-right = [ ];
+          move-to-monitor-up = [ ];
+          move-to-monitor-down = [ ];
+          move-to-workspace-down = [ ];
+          move-to-workspace-up = [ ];
+          switch-to-workspace-down = [ "<Primary><Super>Down" ];
+          switch-to-workspace-up = [ "<Primary><Super>Up" ];
+          switch-to-workspace-left = [ ];
+          switch-to-workspace-right = [ ];
+          move-to-workspace-1 = [ "<Shift><Super>1" ];
+          move-to-workspace-2 = [ "<Shift><Super>2" ];
+          move-to-workspace-3 = [ "<Shift><Super>3" ];
+          move-to-workspace-4 = [ "<Shift><Super>4" ];
+          move-to-workspace-5 = [ "<Shift><Super>5" ];
+          move-to-workspace-6 = [ "<Shift><Super>6" ];
+          move-to-workspace-7 = [ "<Shift><Super>7" ];
+          move-to-workspace-8 = [ "<Shift><Super>8" ];
+          move-to-workspace-9 = [ "<Shift><Super>9" ];
+          switch-to-workspace-1 = [ "<Super>1" ];
+          switch-to-workspace-2 = [ "<Super>2" ];
+          switch-to-workspace-3 = [ "<Super>3" ];
+          switch-to-workspace-4 = [ "<Super>4" ];
+          switch-to-workspace-5 = [ "<Super>5" ];
+          switch-to-workspace-6 = [ "<Super>6" ];
+          switch-to-workspace-7 = [ "<Super>7" ];
+          switch-to-workspace-8 = [ "<Super>8" ];
+          switch-to-workspace-9 = [ "<Super>9" ];
+          maximize = [ ];
+          unmaximize = [ ];
+        };
+        "org/gnome/mutter" = { dynamic-workspaces = false; };
+        "org/gnome/desktop/wm/preferences" = { num-workspaces = 9; };
+
+        "org/gnome/shell/keybindings" = {
+          open-application-menu = [ ];
+          toggle-message-tray = [ "<Super>n" ];
+          toggle-overview = [ ];
+        };
+
+        "org/gnome/mutter/keybindings" = {
+          toggle-tiled-left = [ ];
+          toggle-tiled-right = [ ];
+        };
+
+        "org/gnome/mutter/wayland/keybindings" = { restore-shortcuts = [ ]; };
+
+        "org/gnome/settings-daemon/plugins/media-keys" = {
+          screensaver = [ "<Super>Escape <Super>." ];
+          home = [ "<Super>e" ];
+          www = [ "<Super>b" ];
+          rotate-video-lock-static = [ ];
+        };
+
+        "org/gnome/shell/extensions/pop-shell" = {
+          toggle-tiling = [ "<Super>y" ];
+          toggle-floating = [ "<Super>g" ];
+          tile-enter = [ "<Super>t" ];
+          tile-accept = [ "Return" ];
+          tile-reject = [ "Escape" ];
+          toggle-stacking-global = [ "<Super>s" ];
+          pop-workspace-down = [ "<Shift><Super>Down" "<Shift><Super>j" ];
+          pop-workspace-up = [ "<Shift><Super>Up" "<Shift><Super>k" ];
+          pop-monitor-left = [ "<Shift><Super>Left" "<Shift><Super>h" ];
+          pop-monitor-right = [ "<Shift><Super>Right" "<Shift><Super>l" ];
+          pop-monitor-down = [ ];
+          pop-monitor-up = [ ];
+          focus-left = [ "<Super>Left" "<Super>h" ];
+          focus-down = [ "<Super>Down" "<Super>j" ];
+          focus-up = [ "<Super>Up" "<Super>k" ];
+          focus-right = [ "<Super>Right" "<Super>l" ];
+        };
+      };
     };
 
-    home.stateVersion = "23.11";
+    home.stateVersion = "24.05";
   };
 
   security.sudo.enable = true;
@@ -280,6 +422,5 @@
     echo "updated desktop configs"
   '';
 
-  system.stateVersion = "23.11";
+  system.stateVersion = "24.05";
 }
-
